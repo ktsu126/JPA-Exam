54112671c6774eb0501f441d684f23ccc4b2602d
CraztB <ktsu12660@gmail.com>
2024-05-23 18:18:08 +0900

    JPA 연습 2

--- src/main/java/io/crazyb/jpa/CustomerJpaExam6.java
+++ src/main/java/io/crazyb/jpa/CustomerJpaExam6.java
@@ -1,12 +1,11 @@
 package io.crazyb.jpa;
 
 import io.crazyb.jpa.entity.Customer;
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.EntityManagerFactory;
-import jakarta.persistence.EntityTransaction;
-import jakarta.persistence.Persistence;
+import jakarta.persistence.*;
 
-public class CustomerJpaExam5 {
+import java.util.List;
+
+public class CustomerJpaExam6 {
 
     public static void main(String[] args) {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("customer-exam");
@@ -16,13 +15,15 @@ public class CustomerJpaExam5 {
         tx.begin();
 
         try{
-            Customer customer = new Customer("ID0003","HONG");//비영속상태
+            Customer customer = new Customer("ID0004","Lee");//비영속상태
             em.persist(customer); // customer 객체가 영속상태(Managed)
 
-            em.flush(); // DB와 EM 동기화 commit이 되지않으면 DB에 저장되지않음;
+            //em.flush(); JPQL 실행 시 flush(); 가 실행된다.
+            Query query = em.createQuery("select c from Customer c");
+            List<Customer> customers = query.getResultList();
+            System.out.println(customers);
 
-            Customer foundCustomer = em.find(Customer.class, "ID0003");
-            tx.commit(); //commit 실행 시 flush() 실행됨
+            tx.commit();
         }catch (Exception e){
             tx.rollback();
         }finally {
